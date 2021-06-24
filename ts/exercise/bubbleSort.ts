c13aa6f9a1021c0a3149ec488973fdedaf4ef6a4
dutao <1173127007@qq.com>
2021-06-24 23:43:08 +0800

    feat: bubble sort

---
+++ ts/exercise/bubbleSort.ts
@@ -0,0 +1,17 @@
+function bubbleSort (arr: number[]): number[] {
+  if (arr.length < 2) {
+    return arr;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([2, 3, 1, 6, 12, 46, 1, 53]))
