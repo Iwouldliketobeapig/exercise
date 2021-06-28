4340ad1cf29d5934950d47611bb36cc52ed173bc
dutao <1173127007@qq.com>
2021-06-29 00:07:30 +0800

    feat: insert sort

---
+++ ts/exercise/insertSort.ts
@@ -0,0 +1,22 @@
+// 插入排序，在有序序列中插入为排序元素
+
+import { arrSortFun } from '../types/funTypes';
+
+const insertSort: arrSortFun = function (arr) {
+  const length = arr.length;
+  if (length < 2) {
+    return arr;
+  }
+  for (let i = 0; i < length; i++) {
+    let j = i;
+    let target = arr[j];
+    while (j > 0 && arr[j - 1] > target) {
+      arr[j] = arr[j -1];
+      j--;
+    }
+    arr[j] = target;
+  }
+  return arr;
+}
+
+console.log(insertSort([1, 123, 12, 1, 12, 64]));
--- ts/types/funTypes.ts
+++ ts/types/funTypes.ts
@@ -1,5 +1,7 @@
 export type ArbitaryFun = (...args: any[] | never) => any | void;
 
+export type arrSortFun = (arr: number[]) => number[];
+
 export type anyObject = {
   [name: string]: any
 }
